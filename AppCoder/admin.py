e97fee797e64d41f411428a28b3391152749a538
dfrincong <DIEGO@DESKTOP-OJ7MU1N.testing.com>
2024-06-02 10:15:58 -0500

    Personalizamos el admin

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Curso
+from .models import Profesor
+from .models import Estudiante
+from .models import Entregable
+
+
+class CurstoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'camada']
+    search_fields =['nombre', 'camada']
+    list_filter=['nombre']
 
 # Register your models here.
+admin.site.register(Curso, CurstoAdmin)
+admin.site.register(Profesor)
+admin.site.register(Estudiante)
+admin.site.register(Entregable)
+
--- AppCoder/models.py
+++ AppCoder/models.py
@@ -5,11 +5,21 @@ class Curso (models.Model):
     nombre = models.CharField(max_length=40)
     camada = models.IntegerField()
 
+    def __str__(self):
+        return f'{self.nombre} - {self.camada}'
+    class Meta():
+        verbose_name = 'Course'
+        verbose_name_plural = 'The Courses'
+        ordering= ('nombre','camada')
+        unique_together=('nombre', 'camada')
+
 
 class Estudiante (models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30, null = True)
     email = models.EmailField(null=True)
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
 
 class Profesor(models.Model):
      
@@ -17,8 +27,15 @@ class Profesor(models.Model):
     apellido= models.CharField(max_length=30)
     email= models.EmailField()
     profesion= models.CharField(max_length=30)
+    cursos = models.ManyToManyField(Curso) #m2m es el shortcut para sacar esta línea
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
 
 class Entregable(models.Model):
     nombre= models.CharField(max_length=30)
     fechaDeEntrega= models.DateField()
     Entregado= models.BooleanField()
+    def __str__(self):
+        return f'{self.nombre} '
+    estudiante = models.ForeignKey("Estudiante", on_delete=models.CASCADE)
+    
